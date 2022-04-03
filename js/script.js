ede2dee748ae06e8ccb43dbc2ced5bdcee1e6c8e
Racknar0 <racknarow1@gmail.com>
2022-04-03 00:50:59 -0500

    Modify

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ irArriba(); }); //Hacia arriba
+/* $(document).ready(function(){ irArriba(); }); //Hacia arriba
 
 function irArriba(){
   $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
@@ -6,4 +6,4 @@ function irArriba(){
     if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
   });
   $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
-}
+} */
